994bef25c979b78019cf9514772c5525dc8017b2
joris en luuk <103518111+recepten-van-joris-en-luuk@users.noreply.github.com>
2022-06-13 15:41:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function handleSubmit () {
-    const name = document.getElementById('name').value;
     const surname = document.getElementById('surname').value;
 
     // to set into local storage
     /* localStorage.setItem("NAME", name);
     localStorage.setItem("SURNAME", surname); */
-    
-    sessionStorage.setItem("NAME", name);
     sessionStorage.setItem("SURNAME", surname);
 
     return;
